ac4d9f2beed83c4766c40ec945b7f9ab0000e5be
penguinjing <penguinjing@gmail.com>
2015-12-09 16:30:24 +0800

    hello world 4 Web notetaking programme

---
+++ _src/om2py4w/4wex0/main.py
@@ -0,0 +1,22 @@
+# -*- encoding: utf-8 -*-
+#! /usr/bin/env python
+'''
+文件说明： Web版的极简交互式笔记程序
+作者信息： penguinjing
+版本自述:  0.0.1
+程序参考： None
+'''
+# 全局引用
+from bottle import route, run, template
+
+# 全局变量
+# PATH = "/path/2/work dir"
+# 函式撰写区
+
+#from bottle import route, run, template
+
+@route('/hello/<name>')
+def index(name):
+    return template('<b>Hello {{name}}</b>!', name=name)
+
+run(host='localhost', port=8080)
