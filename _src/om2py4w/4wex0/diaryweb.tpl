33441de0b24cab1615b1a6cd7c83a7f769f781a3
Edward <penguinjing@gmail.com>
2016-01-11 00:36:48 +0800

    web diary program by bottle

---
+++ _src/om2py4w/4wex0/diaryweb.tpl
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+
+    <title>My Diary Web Application</title>
+
+  </head>
+  
+  <body>
+  
+    <form action="/" method="post">
+    Input: <input type="text" name="newdiary" />
+    <input value="Submit" type="submit" />
+    </form><br>
+    <textarea rows="20" cols="50">{{diarylog}}</textarea>
+  
+  </body>
+</html>
--- _src/om2py4w/4wex0/main.py
+++ _src/om2py4w/4wex0/main.py
@@ -3,21 +3,38 @@
 '''
 文件说明： Web版的极简交互式笔记程序
 作者信息： penguinjing
-版本自述:  0.0.1
+版本自述:  0.1.0
 程序参考： None
 '''
 # 全局引用
-from bottle import route, run, template
+from bottle import request, route, run, template
+from time import localtime, strftime
 
 # 全局变量
 # PATH = "/path/2/work dir"
 # 函式撰写区
+def read_diary():
+    f = open('diary log.txt','r')
+    return f.read()
 
-#from bottle import route, run, template
+def write_diary(newdiary):
+    f = open('diary log.txt','a+')
+    edit_time = strftime("%Y %b %d %H:%M:%S", localtime())
+    f.write('%s    %s\n' % (edit_time, newdiary))
+    f.close()
+
 @route('/')
-@route('/hello/<name>')
-def index(name='Stranger'):
-    return template('<b>Hello {{name}}, how are you? </b>!'
-                    , name=name)
+def start():
+    log = read_diary()
+    return template("diaryweb", diarylog=log)
 
-run(host='localhost', port=8080)
+@route('/', method='POST')
+def input_new():
+    newdiary = request.forms.get('newdiary')
+    write_diary(newdiary)
+    log = read_diary()
+    return template("diaryweb", diarylog=log)
+
+
+if __name__ == '__main__':
+    run(host='localhost', port=8255, debug=True, reloader=True)
